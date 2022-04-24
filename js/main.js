36a46e3b86eef5c844d4bb02c7021c9c3d5ef65e
Fernando <fernando-perugachi@hotmail.com>
2022-04-24 15:33:54 -0500

    Update mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const menuNav = document.querySelector(".content-nav");
 const toggleBtn = document.querySelector(".icon-toggle");
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const  menuNavLinks = document.querySelectorAll(".content-nav>a")
+
 
 toggleBtn.addEventListener("click", () =>{
     menuNav.classList.toggle("visibility")
+    openBtn.classList.toggle("hidden-btn")
+    closeBtn.classList.toggle("display-btn")
+})
+
+menuNavLinks.forEach(menuNavLinks =>{
+    menuNavLinks.addEventListener("click", () =>{
+        menuNav.classList.remove("visibility")
+        openBtn.classList.toggle("hidden-btn")
+        closeBtn.classList.toggle("display-btn")
+    })
 })
