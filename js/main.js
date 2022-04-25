8f0f3ed38b6fb879aec21596549a8cca91183da9
Fernando <fernando-perugachi@hotmail.com>
2022-04-24 19:06:15 -0500

    Add modal cart

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ const menuNav = document.querySelector(".content-nav");
 const toggleBtn = document.querySelector(".icon-toggle");
 const openBtn = document.querySelector(".open-btn");
 const closeBtn = document.querySelector(".close-btn");
-const  menuNavLinks = document.querySelectorAll(".content-nav>a")
+const menuNavLinks = document.querySelectorAll(".content-nav>a")
+const cartActiveBtn = document.querySelector(".btn-cart_active");
+const closeModalBtn = document.querySelector(".cart-icon-close")
+const modalShow = document.querySelector(".modal-card")
 
 
 toggleBtn.addEventListener("click", () =>{
@@ -17,4 +20,11 @@ menuNavLinks.forEach(menuNavLinks =>{
         openBtn.classList.toggle("hidden-btn")
         closeBtn.classList.toggle("display-btn")
     })
+})
+
+cartActiveBtn.addEventListener("click", () =>{
+    modalShow.classList.toggle("modal-show")
+})
+closeModalBtn.addEventListener("click", () =>{
+    modalShow.classList.toggle("modal-show")
 })
